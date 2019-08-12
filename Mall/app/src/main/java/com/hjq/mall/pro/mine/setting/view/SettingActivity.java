46929f21a82950dcfdda6a8421c71b8a7134ecfb
HJQ <v-huangjiaqian@midland.local>
2019-08-12 16:55:05 +0800

    增加iOS样式弹窗

--- Mall/app/src/main/java/com/hjq/mall/pro/mine/setting/view/SettingActivity.java
+++ Mall/app/src/main/java/com/hjq/mall/pro/mine/setting/view/SettingActivity.java
@@ -4,7 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.bigkoo.alertview.AlertView;
+import com.bigkoo.alertview.OnItemClickListener;
 import com.blankj.utilcode.util.ToastUtils;
 import com.hjq.mall.R;
 import com.hjq.mall.mvp.presenter.impl.MvpBasePresenter;
@@ -16,10 +19,34 @@ import com.hjq.mall.pro.base.view.navigation.CommonNavigationBuilder;
  */
 public class SettingActivity extends BaseActivtiy {
 
+    private Button mLogoutBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initToolBar(getWindow().getDecorView().findViewById(R.id.fl_layout));
+        initView();
+    }
+
+    private void initView() {
+        initToolBar(getWindow().getDecorView().findViewById(R.id.fl_toolbar));
+        mLogoutBtn = findViewById(R.id.btn_logout);
+        mLogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLogoutAlert();
+            }
+        });
+
+    }
+
+    private void showLogoutAlert() {
+        new AlertView("提示", "您确定要退出吗？", null, null,
+                new String[]{"取消", "确定"},
+                this, AlertView.Style.Alert, new OnItemClickListener(){
+            public void onItemClick(Object o,int position){
+                ToastUtils.showShort("点击了第" + position + "个");
+            }
+        }).show();
     }
 
     @Override
